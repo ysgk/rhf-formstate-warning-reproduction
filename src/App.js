5d07fb3b9f556e8a9a000ce2b2f36f7ca75f77b8
ysgk <ysgk@users.noreply.github.com>
2020-11-01 18:47:03 +0900

    use useForm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form'
 
 function App() {
+  const { register, formState } = useForm({ mode: 'onChange' })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <input name={'email'} type={'email'} ref={register({ required: true })} />
+      <button disabled={!formState.isValid}>Submit</button>
+    </form>
   );
 }
 
